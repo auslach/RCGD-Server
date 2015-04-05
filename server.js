eac320a2e5552d136e9cf2350ee8a6ddd139e465
Mozoby <brthornbury@csupomona.edu>
2015-04-05 08:20:26 -0700

    Data Buffer concept added.

--- server.js
+++ server.js
@@ -5,22 +5,42 @@ var PORT = 6969;
 var phoneActive = null;
 
 net.createServer(function (socket) {
-  socket.on('data', function (data) {
-    var json = JSON.parse(data.toString());
-    // data will be either P or C
-    if (json['clientType'] == "c") {
-      console.log('Server has heard from client');
-      // if phone connection is active
-      if (phoneActive != null) {
-        // client is client, push data to open phone connection
-        console.log("phone is connected");
-        phoneActive.write(data);
+
+  var dataBuffer = "";
+
+  function processDataBuffer(){
+
+    var dataItems = dataBuffer.toString().split('\n')
+
+    if(dataItems.length >= 2){
+      //we have a processable data item
+      for(var i = 0; i < dataItems.length - 1; ++i){
+        json = JSON.parse(dataItems[i].toString())
+        console.log(json.toString())
+        // data will be either P or C
+        if (json['clientType'] == "c") {
+          console.log('Server has heard from client');
+          // if phone connection is active
+          if (phoneActive != null) {
+            // client is client, push data to open phone connection
+            console.log("phone is connected");
+            phoneActive.write(data);
+          }
+        } else if (json['clientType'] == "p") {
+          console.log('Server has heard from phone');
+          // client is a phone, set phone var to socket
+          phoneActive = socket;
+        }
       }
-    } else if (json['clientType'] == "p") {
-      console.log('Server has heard from phone');
-      // client is a phone, set phone var to socket
-      phoneActive = socket;
     }
+  }
+
+  socket.on('data', function (data) {
+    //console.log(data);
+
+    dataBuffer += data
+    processDataBuffer();
+    
   });
 
   socket.on('close', function(data) {
@@ -32,6 +52,12 @@ net.createServer(function (socket) {
       console.log("Closing connection to client..");
     }
   });
+
+  socket.on('error', function(data){
+    console.log("ERRORS MOTHERFUCKER!!!!");
+    console.log(data);
+  });
+
 }).listen(PORT, HOST);
 
 console.log("Listening on port " + PORT);
