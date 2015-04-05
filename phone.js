9930cf7ab48e23f8b5245c81d9ceeb59b422173d
Austin L. Chang <austinlchang@gmail.com>
2015-04-05 02:48:22 -0700

    working server w/ test client and phone connections

--- phone.js
+++ phone.js
@@ -6,12 +6,13 @@ var PORT = 6969;
 var client = new net.Socket();
 
 client.connect(PORT, HOST, function () {
-  var data = { id: 'L', val: 32000, clientType: "c" };
+  var data = { connected: true, clientType: "p" };
   var json = JSON.stringify(data);
   client.write(json);
 
+  // test, kill phone connection after 5 sec
   setTimeout(function () {
     client.destroy();
-  }, 1000);
+  }, 5000);
 });
 
--- server.js
+++ server.js
@@ -2,19 +2,36 @@ var net = require('net');
 
 var HOST = '127.0.0.1';
 var PORT = 6969;
+var phoneActive = null;
 
 net.createServer(function (socket) {
-  console.log('Client has connected to server');
-
-  socket.on(['data', 'client'], function (data) {
-    json = JSON.parse(data.toString());
-    console.log(json);
+  socket.on('data', function (data) {
+    var json = JSON.parse(data.toString());
+    // data will be either P or C
+    if (json['clientType'] == "c") {
+      console.log('Server has heard from client');
+      // if phone connection is active
+      if (phoneActive != null) {
+        // client is client, push data to open phone connection
+        console.log("phone is connected");
+        phoneActive.write(data);
+      }
+    } else if (json['clientType'] == "p") {
+      console.log('Server has heard from phone');
+      // client is a phone, set phone var to socket
+      phoneActive = socket;
+    }
   });
 
-  socket.on('close', function (data) {
-    console.log('Connection has closed');
+  socket.on('close', function(data) {
+    // if socket is phone, unset phone var
+    if (socket == phoneActive) {
+      phoneActive = null;
+      console.log("Closing connection to phone..");
+    } else {
+      console.log("Closing connection to client..");
+    }
   });
-
 }).listen(PORT, HOST);
 
 console.log("Listening on port " + PORT);
